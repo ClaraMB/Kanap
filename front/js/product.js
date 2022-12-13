e723d5233e0c5aabc82614220a765dbbfc7f52ce
Clara Maringe <claramaringe@gmail.com>
2022-12-13 18:47:58 +0100

    Ajout fichiers

--- front/js/product.js
+++ front/js/product.js
@@ -1,18 +1,10 @@
-fetch("http://localhost:3000/api/products") // Appel à l'API pour récupérer sous forme de tableau la liste des produits
+fetch("http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926") // Appel à l'API pour récupérer sous forme de tableau la liste des produits
 .then(function(res) { // Réponse du backend sous format json
       return res.json(); // Retour du backend 
 })
-  .then(tableaudekanap => { // On stock le résultat dans une variable. récupération grâce à l'id situé dans le DOM
-    const section = document.getElementById("items"); // On ajoute de l'html en conservant toutes les lignes dans la balise section qui a pour id items
-    tableaudekanap.forEach(kanap => {
-    section.innerHTML += `<a href="./product.html?id=` + kanap._id + `"> 
-    <article>
-    <img src="` + kanap.imageUrl + `" alt="` + kanap.altTxt + `">
-    <h3 class="productName">` + kanap.name + `</h3>
-    <p class="productDescription">` + kanap.description + `</p>
-    </article>
-</a> `;
-    });
+  .then(data => { // On stock le résultat dans une variable. récupération grâce à l'id situé dans le DOM
+    const div = document.querySelector(".item__img"); // On ajoute de l'html en conservant toutes les lignes dans la balise section qui a pour id items
+    div.innerHTML += `<img src="` + data.imageUrl + `" alt="` + data.altTxt + `"> `;
 })
 .catch(function(err) {
     // Une erreur est survenue
