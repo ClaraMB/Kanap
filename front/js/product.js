38f3ab5da8ac7d2405f490ab0c7e98abf30c7878
Clara Maringe <claramaringe@gmail.com>
2022-12-26 18:45:42 +0100

    Ajout page product.js et panier.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,11 +1,30 @@
-fetch("http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926") // Appel à l'API pour récupérer sous forme de tableau la liste des produits
+const searchParams = new URLSearchParams(window.location.search);
+const id = searchParams.get('id');
+
+
+fetch("http://localhost:3000/api/products/" + id) // Appel à l'API pour récupérer l'image
 .then(function(res) { // Réponse du backend sous format json
       return res.json(); // Retour du backend 
 })
-  .then(data => { // On stock le résultat dans une variable. récupération grâce à l'id situé dans le DOM
-    const div = document.querySelector(".item__img"); // On ajoute de l'html en conservant toutes les lignes dans la balise section qui a pour id items
-    div.innerHTML += `<img src="` + data.imageUrl + `" alt="` + data.altTxt + `"> `;
+.then(data => { // On stock le résultat dans une variable. récupération grâce à query selector
+    const image = document.querySelector(".item__img"); 
+    image.innerHTML = `<img src="` + data.imageUrl + `" alt="` + data.altTxt + `"> `;
+
+    const name = document.querySelector("#title"); 
+    name.innerHTML = `<h1 id="title">` + data.name + `</h1>`;
+
+    const price = document.querySelector("#price"); 
+    price.innerHTML = `<span id="price">` + data.price + `</span>`;
+
+    const div = document.querySelector(".item__content__description"); 
+    div.innerHTML = `<p id="description">` + data.description + ` </p>`;
+
+    const select = document.querySelector("#colors"); 
+    data.colors.forEach(color => {
+        select.innerHTML += `<option value="` + color + `">` + color + `</option>`;
+        });
 })
+
 .catch(function(err) {
     // Une erreur est survenue
-});
+})
